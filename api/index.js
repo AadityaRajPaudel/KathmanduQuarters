b07bb113f7c8fe7aa0f5393564cad676e648b471
AadityaRajPaudel <aadityapaudel834@gmail.com>
2025-01-01 20:59:26 +0545

    fix: dirname

--- api/index.js
+++ api/index.js
@@ -23,6 +23,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+const __dirname = path.resolve();
 
 app.listen(process.env.PORT, () => {
   console.log("Server connected on port 3000...");
@@ -33,7 +34,7 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
